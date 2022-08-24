04995e2659811e27366c234a8b43f038dd9a429a
HyperCode <72990129+Bit-Han@users.noreply.github.com>
2022-08-24 01:46:23 +0100

    setup commit

---
+++ src/components/Biling.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Biling = () => {
+	return <div>Biling</div>;
+};
+
+export default Biling;
---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Button from "./Button";
+import Biling from "./Biling";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Business from "./Business";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+    Navbar,
+    Biling,
+    CardDeal,
+    Business,
+    Clients,
+    Button,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+};
